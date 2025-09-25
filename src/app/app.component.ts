c14419776982b2799927b247678a04cf578828f8
Louis Policarpio <94048557+locwa@users.noreply.github.com>
2025-09-25 12:01:55 +0800

    added components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { SearchResultsComponent } from './search-results/search-results.component'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SearchBarComponent, SearchResultsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
